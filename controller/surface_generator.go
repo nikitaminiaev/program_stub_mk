b9cb9955ec6543bbd64f60b5cabfd500ed9b3e27
N.Minayev <minyaev@jivosite.com>
2022-12-14 16:45:10 +0400

    Refactoring

--- controller/surface_generator.go
+++ controller/surface_generator.go
@@ -1,4 +1,13 @@
 package controller
 
 type SurfaceGenerator struct {
+	Surface [surfaceSize][surfaceSize]uint16
+}
+
+func (sg *SurfaceGenerator) GenSurface(height uint16) {
+	for x, column := range sg.Surface {
+		for y := range column {
+			sg.Surface[x][y] = height
+		}
+	}
 }
